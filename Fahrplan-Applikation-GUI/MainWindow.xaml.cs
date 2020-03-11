7aacd34ad587fa65a0cc39df06fe7dd25b9c78a9
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-03-11 08:51:18 +0100

    Fahrplan und Stationensuche

--- Fahrplan-Applikation-GUI/MainWindow.xaml.cs
+++ Fahrplan-Applikation-GUI/MainWindow.xaml.cs
@@ -12,14 +12,87 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using SwissTransport;
 namespace Fahrplan_Applikation_GUI {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        Transport transport;
+        NavigationEngine navigationEngine;
+
+        Connections fahrplanConnections { get { return fahrplanConnections; } set { fahrplanConnections = value; } }
         public MainWindow() {
             InitializeComponent();
+            transport = new Transport();
+            List<Grid> menuEntries = new List<Grid>() { fahrplanGrid, abfahrtstafelGrid, stationensucheGrid, stationenBeiMirGrid };
+            navigationEngine = new NavigationEngine(menuEntries, "fahrplan", titleLabel);
+        }
+
+        private void onFahrplanSearchClick(object sender, RoutedEventArgs e) {
+            if (vonFahrplanComboBox.Text.Length > 0 && bisFahrplanComboBox.Text.Length > 0) {
+                Connections connections = transport.GetConnections(vonFahrplanComboBox.Text, bisFahrplanComboBox.Text);
+                if (connections.ConnectionList.Count > 0) {
+
+                    List<string> connectionsStringList = new List<string>();
+                    for (int i = 0; i < 5; i++) {
+                        if (i < connections.ConnectionList.Count) {
+                            string outStr;
+                            Connection el = connections.ConnectionList.ElementAt(i);
+                            outStr = el.From.Station.Name + " (" + DateTime.Parse(el.From.Departure).ToShortTimeString() + ")";
+                            outStr += " -> ";
+                            outStr += el.To.Station.Name + " (" + DateTime.Parse(el.To.Arrival).ToShortTimeString() + ")";
+                            outStr += " - (" + el.Duration + ")";
+                            connectionsStringList.Add(outStr);
+                        }
+                    }
+                    fahrplanListBox.ItemsSource = connectionsStringList;
+                } else {
+                    showError("Es wurden keine Resultate gefunden.");
+                }
+            } else {
+                showError("Bitte geben Sie eine Abfharts- und Ankunftsstation an.");
+            }
+        }
+        private List<string> searchStationName(String str) {
+            List<Station> stationList = transport.GetStations(str).StationList;
+            List<string> stationNameList = new List<string>();
+            foreach (Station s in stationList) {
+                stationNameList.Add(s.Name);
+            }
+            return stationNameList;
+        }
+
+        private void onNavButtonClick(object sender, RoutedEventArgs e) {
+            if (sender is Button) {
+                Button b = (Button)sender;
+                navigationEngine.setActiveGrid(b.Name);
+            } else {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void onStationensucheSucheClick(object sender, RoutedEventArgs e) {
+            if (stationensucheTextBox.Text.Length != 0) {
+                List<string> stations = searchStationName(stationensucheTextBox.Text);
+                if (stations.Count > 0) {
+                    stationensucheListBox.ItemsSource = stations;
+                } else {
+                    stationensucheListBox.Items.Clear();
+                    showError("Die gewünschte Abfrage hat keine Resultate geliefert.");
+                }
+            } else {
+                showError("Bitte geben Sie eine Suche ein.");
+            }
+        }
+
+        private void showError(string message) {
+            MessageBox.Show(message, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void stationensucheTextBoxKeyUp(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter)
+                onStationensucheSucheClick(sender, null);
         }
     }
 }
---
+++ Fahrplan-Applikation-GUI/NavigationEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Fahrplan_Applikation_GUI {
+    class NavigationEngine {
+        private List<Grid> menuEntries = new List<Grid>();
+        public Grid activeGrid;
+        private Label currentPageLabel;
+        public NavigationEngine(List<Grid> menuEntries, string startGrid, Label currentPageLabel) {
+            this.menuEntries = menuEntries;
+            this.currentPageLabel = currentPageLabel;
+            setActiveGrid(startGrid);
+        }
+
+        public void setActiveGrid(string buttonName) {
+            Grid gridToSelect = getGridFromString(buttonName);
+            foreach(Grid g in menuEntries) {
+                g.Visibility = System.Windows.Visibility.Hidden;
+            }
+            gridToSelect.Visibility = System.Windows.Visibility.Visible;
+            activeGrid = gridToSelect;
+            currentPageLabel.Content = gridToSelect.Name;
+        }
+
+        private Grid getGridFromString(string name) {
+            Grid grid = null;
+            foreach(Grid iterativeGrid in menuEntries) {
+                if(iterativeGrid.Name == (name + "Grid")) {
+                    grid = iterativeGrid;
+                }
+            }
+            if(grid != null) {
+                return grid;
+            } else {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+    }
+}
